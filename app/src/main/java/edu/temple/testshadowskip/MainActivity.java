5313e6724ecc72d94a8e4e7bd019f85126d13055
Braeden Z Trautz <braedentrautz@gmail.com>
2021-04-07 15:20:58 -0400

    Incorporated time sensitive activation for gyroscope.  Phone must be stable for 5 seconds to be considered "ready" now.

--- app/src/main/java/edu/temple/testshadowskip/MainActivity.java
+++ app/src/main/java/edu/temple/testshadowskip/MainActivity.java
@@ -9,9 +9,12 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -19,6 +22,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Sensor gyroscope;
     private Sensor accelerometer;
     private Sensor magneticField;
+    private long gyroStart;
+    private long elapsedSeconds;
+    private TextView proxStat;
+    private TextView gyroStat;
+    private TextView orienStat;
+
 
 
     @Override
@@ -31,9 +40,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Button gyroscopeButton = findViewById(R.id.btn_gyro);
         Button orientationButton = findViewById(R.id.btn_orientation);
         Button proximityButton = findViewById(R.id.btn_proximity);
-        TextView gyroStat = findViewById(R.id.txt_gyro_stat);
-        TextView orienStat = findViewById(R.id.txt_orien_stat);
-        TextView proxStat = findViewById(R.id.txt_prox_stat);
+        gyroStat = findViewById(R.id.txt_gyro_stat);
+        orienStat = findViewById(R.id.txt_orien_stat);
+        proxStat = findViewById(R.id.txt_prox_stat);
 
 
         if(sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null) {
@@ -66,12 +75,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             });
         }
 
+        gyroStart = (long) 0;
+        elapsedSeconds = 0;
+
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         magneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
-
+        sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
 
     }
 
@@ -88,6 +100,36 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     public void gyroChanged(SensorEvent event){
+        long time = event.timestamp;
+        long seconds = time / (long)1000000000;
+//        Log.println(Log.ASSERT, "Time", String.valueOf(seconds));
+        float xValue = event.values[0];
+        float yValue = event.values[1];
+        float zValue = event.values[2];
+        float avg = (xValue + yValue + zValue) / 3;
+
+        if(avg < 0.01 && avg > -0.01 && elapsedSeconds == 0) {
+            gyroStart = seconds;
+            elapsedSeconds = 1;
+        }
+        else if (avg < 0.01 && avg > -0.01 && elapsedSeconds != 0){
+            elapsedSeconds = seconds - gyroStart;
+        }
+
+        if (elapsedSeconds > 5){
+            gyroStat.setText("Ready");
+            elapsedSeconds = seconds - gyroStart;
+        }
+
+        if (!(avg < 0.01 && avg > -0.01)){
+            gyroStat.setText("Not Ready");
+            gyroStart = 0;
+            elapsedSeconds = 0;
+        }
+
+        Log.println(Log.ASSERT, "elapsed", String.valueOf(elapsedSeconds));
+        Log.println(Log.ASSERT, "gyroStart", String.valueOf(gyroStart));
+
 
     }
 }
